fc4e7c54f9a76b0733a59bfb019e238aa551dbca
rodirefill <r_blomman_14@hotmail.com>
2024-05-08 10:51:03 +0200

    uu

--- htdocs/actions/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('../../include/mellivora.inc.php');
-
-enforce_authentication();
-
-validate_xsrf_token(array_get($_POST, CONST_XSRF_TOKEN_KEY));
-
-logout();
--- htdocs/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require('../include/mellivora.inc.php');
-
-redirect(Config::get('MELLIVORA_CONFIG_INDEX_REDIRECT_TO'));
